79d94cc76f1236ba1e1abf23a2c541b236f7b8bf
Mohammedi Abdenasser <75934365+mdinacer@users.noreply.github.com>
2022-12-29 21:02:38 +0100

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'media.graphassets.com',
+      'media.graphcms.com',
+      's.gravatar.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
+};
